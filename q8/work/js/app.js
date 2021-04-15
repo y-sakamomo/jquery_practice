6d08af0cf7fb7b0a047b14c4aaecfba8442afbe8
y-sakamo <yuki.sakamo.v@gmail.com>
2021-04-15 21:11:09 +0900

    質問プッシュ④

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -1,24 +1,14 @@
 $(function () {
-  // 入力した内容をsearchWordに代入
-  const searchWord = $("#search-input").val();
-  // pageCountの初期値をを１にする
-  let pageCount = 1;
-  // // タイトル
-  // items.title
-  // // 作者
-  // items["dc:creator"];
-  // // 出版社
-  // items["dc:publisher"];
-  // // 書籍情報
-  // items.link["@id"];
   // .listsクラスに該当の書籍一覧を表示させる処理
-  // function displayResult() {
-  //   // @graph配列の中のitems配列の中身を個別に取り出し処理を指定する
-  //   for (items of ['@graph']) {
-  //     // 部分一致したものを.listsクラスに追加し表示する
-  //     $(".lists").prepend(`<li class="lists-item"><div class="list-inner"><p>タイトル：</p><p>作者：</p><p>出版社：</p><a href="">書籍情報</a></div></li>`)
-  //   }
-  // }
+  function displayResult(r) {
+    // .messageクラスを取り除く
+    $(".message").remove();
+    // resultの中のitems配列の中身を個別に取り出し処理を指定する
+    $.each(r[0].items, function (index) {
+      // 部分一致したものを.listsクラスに追加し表示する
+      $(".lists").prepend(`<li class="lists-item"><div class="list-inner"><p>タイトル：${r[0].items[index].title}</p><p>作者：${r[0].items[index]["dc:creator"]}</p><p>出版社：${r[0].items[index]["dc:publisher"]}</p><a href="${r[0].items[index]["@id"]}">書籍情報</a></div></li>`)
+    })
+  }
   // エラーメッセージを表示させる処理
   function displayError() {
     // .listsクラスを空にする
@@ -26,13 +16,18 @@ $(function () {
     // エラーメッセージを.listsクラスの直前に追加する
     $(".lists").before('<div class="message">正常に通信できませんでした。<br>インターネットの接続の確認をしてください。</div>')
   }
+  // pageCountの初期値をを1にする
+  let pageCount = 1;
   // .search-btnをクリックした時、
   $(".search-btn").on("click", function () {
-    // 検索ワードが同じ場合、pageCountを+1にする。同じでない場合、pageCountのを１し、.listsを空にする。
-    if (searchWord === searchWord) {
-      pageCount++;
+    // 入力した内容をsearchWordに代入
+    const searchWord = $("#search-input").val();
+    // 検索ワードが入力されたままの場合は、pageCountに+1する。そうでない以外の場合は1に戻し.listsを空にする。
+    if(searchWord !== "") {
+      $(".lists").empty(),
+      pageCount = 1;
     } else {
-      pageCount = 1, $(".lists").empty();
+      pageCount++;
     }
     // 変数settingsに設定情報などを格納
     const settings = {
@@ -46,13 +41,14 @@ $(function () {
       const result = response['@graph'];
       // displayResultの実行
       displayResult(result)
+      console.log(result);
+      console.log(result[0].items);
+      console.log(pageCount);
       // データがダウンロードできなかったときの処理
     }).fail(function (err) {
       // displayErrorの実行
       displayError(err)
     })
-    console.log(['@graph']);
-    console.log(['@graph.items.title']);
   });
   // リセットボタンの機能実装
   $(".reset-btn").on("click", function () {
@@ -62,5 +58,7 @@ $(function () {
     $(".lists").empty();
     // 検索ワードに入力された内容を空にする
     $("#search-input").val("");
+    // pageCountを0に戻す
+    pageCount = 0;
   })
 });
